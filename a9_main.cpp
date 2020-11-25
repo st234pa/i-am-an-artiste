87f43020baa38b2d9a45aa77c012d2e1ff74845a
st234pa <stephanie.yoon123@gmail.com>
2020-11-24 19:38:56 -0500

    added example, added Eigen, added instruction

--- a9_main.cpp
+++ a9_main.cpp
@@ -4,83 +4,6 @@
 
 using namespace std;
 
-void testSingleScale()
-{
-  Image Liz("./Input/liz.png");
-  Image out(Liz.width(), Liz.height(), Liz.channels());
-  Image importance(Liz.width(), Liz.height(), Liz.channels());
-  for (int x = 0; x < importance.number_of_elements(); x++)
-  {
-    importance(x) = 1.0f;
-  }
-  Image texture("./Input/brush.png");
-  singleScalePaint(Liz, out, importance, texture);
-  out.write("./Output/liz_single_scale.png");
-};
-
-void testPainterly()
-{
-  Image villeperdue("./Input/villeperdue.png");
-  Image texture("./Input/brush.png");
-  Image out = painterly(villeperdue, texture);
-  out.write("./Output/villeperdue_painterly.png");
-}
-
-void testAngle()
-{
-  Vec2f v;
-  v << 1.0f, 0.0f;
-  cout << horizontalAngle(v) << endl;
-  v << 0.0f, 1.0f;
-  cout << horizontalAngle(v) << endl;
-  v << -1.0f, -0.0f;
-  cout << horizontalAngle(v) << endl;
-  v << 0.0f, -1.0f;
-  cout << horizontalAngle(v) << endl;
-}
-
-void testRotate()
-{
-  Image texture("./Input/brush.png");
-  Image out = rotate(texture, 3.0f);
-  out.write("./Output/brush_rotated.png");
-}
-
-void testComputeAngles()
-{
-  Image villePerdue("./Input/villeperdue.png");
-  Image out(villePerdue.width(), villePerdue.height(), villePerdue.channels());
-  Image importance(villePerdue.width(), villePerdue.height(), villePerdue.channels());
-  for (int x = 0; x < importance.number_of_elements(); x++)
-  {
-    importance(x) = 1.0f;
-  }
-  Image texture("./Input/longBrush2.png");
-  Image thetas = computeAngles(villePerdue);
-  for (int p = 0; p < thetas.number_of_elements(); p++)
-  {
-    if (thetas(p) != 0)
-    {
-      cout << thetas(p) << endl;
-    }
-  }
-}
-
-void testSingleScaleOriented()
-{
-  Image villeperdue("./Input/villeperdue.png");
-  Image out(villeperdue.width(), villeperdue.height(), villeperdue.channels());
-  Image importance(villeperdue.width(), villeperdue.height(), villeperdue.channels());
-  for (int x = 0; x < importance.number_of_elements(); x++)
-  {
-    importance(x) = 1.0f;
-  }
-  Image texture("./Input/longBrush2.png");
-  Image thetas = computeAngles(villeperdue);
-  singleScaleOrientedPaint(villeperdue, out, thetas, importance, texture, 50, 10000, 0.3f);
-  out.write("./Output/villeperdue_single_scale_oriented.png");
-}
-
 void testOriented(string in, string out, int size = 50, int N = 7000, float noise = 0.3f)
 {
   Image texture("./Input/longBrush2.png");
@@ -90,6 +13,7 @@ void testOriented(string in, string out, int size = 50, int N = 7000, float nois
 
 int main()
 {
-  testOriented("./Input/linda2.png", "./Output/linda2_painted.png", 50, 80000, 0.3f);
+  // name of input file, name of output file, brush stroke size (default is 50), number of strokes (default is 7000), amount of random color variation (default is 0.3f)
+  testOriented("./Input/saena_color.png", "./Output/saena_color_painted.png", 25, 50000, 0.3f);
   return EXIT_SUCCESS;
 }
